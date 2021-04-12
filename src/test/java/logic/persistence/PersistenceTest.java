3bba339099741e1dfad34bcfb36dd1e3798cfe95
Dustin <eiki300499@gmail.com>
2021-04-12 23:06:41 +0200

    added JBP

--- src/test/java/logic/persistence/PersistenceTest.java
+++ src/test/java/logic/persistence/PersistenceTest.java
@@ -41,6 +41,32 @@ class PersistenceTest {
     }
 
     @Test
+    void testGood_JBP_SaveAndLoadStorage() {
+        BusinessLogic bl1 = new BusinessLogic(new StorageContent(new BigDecimal("123456")));
+        BusinessLogic bl2 = new BusinessLogic(new StorageContent(new BigDecimal("123456")));
+        PersistenceStorage persist1 = new PersistenceStorage(bl1);
+        PersistenceStorage persist2 = new PersistenceStorage(bl2);
+
+        try {
+            bl1.uploadContent(
+                    MediaType.LicensedAudioVideo, 1, 1, 1, "ABC", "CBS", 2, Duration.ofSeconds(231), null, (new UploaderImpl("UPU")), "Abstimmung");
+        } catch (SizeReachedException e) {
+            Assertions.fail();
+        }
+        try {
+            persist1.saveJBP();
+            if (!bl2.getStorage().getListContent().isEmpty()) {
+                Assertions.fail();
+            }
+            persist2.loadJBP();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertFalse(bl2.getStorage().getListContent().isEmpty());
+        Assertions.assertEquals(1, bl2.getStorage().getListContent().size());
+    }
+
+    @Test
     void testGood_SaveAndLoadContent() {
         BusinessLogic bl1 = new BusinessLogic(new StorageContent(new BigDecimal("123456")));
         BusinessLogic bl2 = new BusinessLogic(new StorageContent(new BigDecimal("123456")));
