0ff5ceb586b56e3c07bbd6d700049c7fb9e3237a
Dokriseum <eiki300499@gmail.com>
2021-04-12 18:25:17 +0200

    ++persistence + tests

--- src/test/java/logic/persistence/PersistenceTest.java
+++ src/test/java/logic/persistence/PersistenceTest.java
@@ -2,25 +2,24 @@ package logic.persistence;
 
 import exceptions.SizeReachedException;
 import logic.BusinessLogic;
-import logic.event.InputEventPersistence;
-import logic.event.listener.InputEventListenerPersistence;
 import models.mediaDB.UploaderImpl;
 import models.storage.MediaType;
 import models.storage.StorageContent;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.Duration;
 
 class PersistenceTest {
 
     @Test
-    void testGoodSaveStorage() {
+    void testGood_JOS_SaveAndLoadStorage() {
         BusinessLogic bl1 = new BusinessLogic(new StorageContent(new BigDecimal("123456")));
         BusinessLogic bl2 = new BusinessLogic(new StorageContent(new BigDecimal("123456")));
-        InputEventListenerPersistence listener1 = new InputEventListenerPersistence(bl1);
-        listener1.onInputEvent(new InputEventPersistence(this, "InputEventPersistence", PersistenceType.SAVE_JOS));
+        PersistenceStorage persist1 = new PersistenceStorage(bl1);
+        PersistenceStorage persist2 = new PersistenceStorage(bl2);
 
         try {
             bl1.uploadContent(
@@ -28,10 +27,43 @@ class PersistenceTest {
         } catch (SizeReachedException e) {
             Assertions.fail();
         }
-        InputEventListenerPersistence listener2 = new InputEventListenerPersistence(bl2);
-        listener2.onInputEvent(new InputEventPersistence(this, "InputEventPersistence", PersistenceType.LOAD_JOS));
-        //Assertions.assertFalse(bl2.getStorage().getListContent().isEmpty());
-        System.out.println(bl2.getFiles());
+        try {
+            persist1.saveJOS();
+            if (!bl2.getStorage().getListContent().isEmpty()) {
+                Assertions.fail();
+            }
+            persist2.loadJOS();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertFalse(bl2.getStorage().getListContent().isEmpty());
+        Assertions.assertEquals(1, bl2.getStorage().getListContent().size());
+    }
+
+    @Test
+    void testGood_SaveAndLoadContent() {
+        BusinessLogic bl1 = new BusinessLogic(new StorageContent(new BigDecimal("123456")));
+        BusinessLogic bl2 = new BusinessLogic(new StorageContent(new BigDecimal("123456")));
+        PersistenceStorage persist1 = new PersistenceStorage(bl1);
+        PersistenceStorage persist2 = new PersistenceStorage(bl2);
+        String address;
+        try {
+            bl1.uploadContent(
+                    MediaType.LicensedAudioVideo, 1, 1, 1, "ABC", "CBS", 2, Duration.ofSeconds(231), null, (new UploaderImpl("UPU")), "Abstimmung");
+        } catch (SizeReachedException e) {
+            Assertions.fail();
+        }
+        address = bl1.getFile(0).getAddress();
+        try {
+            persist1.saveContent(address);
+            if (!bl2.getStorage().getListContent().isEmpty()) {
+                Assertions.fail();
+            }
+            persist2.loadContent(address);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertEquals(address, bl2.getFile(0).getAddress());
     }
 
 }
